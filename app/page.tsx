428aa85547f63651b1d3c1e941befd958798f38c
Jakub Figlak <jakub.figlak@gmail.com>
2024-07-26 15:43:28 +0200

    feat: add shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <p>Hello Lingo!</p>
+      <Button size="lg" variant="destructive">
+        Click me!
+      </Button>
     </div>
   );
 }
