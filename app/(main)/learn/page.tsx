5d013a0124ff2455e01b72da8eee10b8f8b0b627
Jakub Figlak <jakub.figlak@gmail.com>
2024-07-27 23:45:01 +0200

    feat: learn page layout

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,24 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./_components/header";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPage = () => {
-  return <div>learn page</div>;
+  return (
+    <div className="flex gap-[48px] px-6">
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
---
+++ components/user-progress.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { InfinityIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+type UserProgressProps = {
+  activeCourse: { title: string; imageSrc: string }; // TODO: replace with db types
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+};
+
+export const UserProgress = ({
+  activeCourse,
+  hearts,
+  points,
+  hasActiveSubscription,
+}: UserProgressProps) => {
+  return (
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <Link href="/courses">
+        <Button variant="ghost">
+          <Image
+            src={activeCourse.imageSrc}
+            alt={activeCourse.imageSrc}
+            width={32}
+            height={32}
+            className="rounded-md border"
+          />
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-orange-500">
+          <Image
+            src="/points.svg"
+            alt="points"
+            width={28}
+            height={28}
+            className="mr-2"
+          />
+          {points}
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-rose-500">
+          <Image
+            src="/heart.svg"
+            alt="hearts"
+            width={28}
+            height={28}
+            className="mr-2"
+          />
+          {hasActiveSubscription ? (
+            <InfinityIcon className="h-4 w-4 stroke-[3]" />
+          ) : (
+            hearts
+          )}
+        </Button>
+      </Link>
+    </div>
+  );
+};
